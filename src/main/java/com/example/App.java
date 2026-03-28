4b723afae3055ae01d41afe8454b5aa5f21eaa8d
chris <christopherzhang1111@gmail.com>
2026-03-28 14:39:18 -0700

    use firefox profile

--- src/main/java/com/example/App.java
+++ src/main/java/com/example/App.java
@@ -1,33 +1,46 @@
 package com.example;
 
+import io.github.cdimascio.dotenv.Dotenv;
+import java.time.Duration;
+import java.util.Arrays;
+import java.util.List;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.support.ui.*;
-import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.WebElement;
-import java.time.Duration;
-import java.util.List;
-import java.util.Arrays;
-import io.github.cdimascio.dotenv.Dotenv;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.firefox.FirefoxOptions;
+import org.openqa.selenium.support.ui.*;
 
 //mvn compile ;  mvn exec:java -Dexec.mainClass="com.example.App"
 public class App {
+
     static String fileName = "test.json";
     static WebDriver driver;
+    static Dotenv dotenv;
 
     public static void main(String[] args) {
-        driver = new FirefoxDriver();
+        dotenv = Dotenv.load();
+
+        FirefoxOptions options = new FirefoxOptions();
+
+        String profilePath = dotenv.get("FIREFOX_PROFILE_PATH");
+        options.addArguments("-profile", profilePath);
+
+        driver = new FirefoxDriver(options);
         driver.get(getBankURL(1));
 
-        login();
+        By accountButtonBy = By.xpath(
+            "//div[@data-button-type='multipleChoiceIdentifier']"
+        );
+        WebElement accountButton = waitFor(accountButtonBy);
+        accountButton.click();
 
         int numQuestions = 5;
-        for (int i = 0; i < numQuestions; i++){
+        for (int i = 0; i < numQuestions; i++) {
             populateQuestion(i);
         }
 
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(30));
-
     }
 
     public static WebElement waitFor(By thing) {
@@ -35,51 +48,39 @@ public class App {
         return wait.until(ExpectedConditions.visibilityOfElementLocated(thing));
     }
 
-    public static void sleep(int seconds){
-        try{
+    public static void sleep(int seconds) {
+        try {
             Thread.sleep(seconds * 1000);
-        }catch(InterruptedException error){
-
-        }
+        } catch (InterruptedException error) {}
     }
 
     public static void login() {
-        Dotenv dotenv = Dotenv.load();
-        String email = dotenv.get("EMAIL");
-        String pass = dotenv.get("PASS");
-
-        //enter email
-        WebElement emailInput = waitFor(By.id("identifierId"));
-        emailInput.sendKeys(email);
-
-        //click next
-        sleep(1);
-        driver.findElement(By.xpath("//span[text()='Next']/..")).click();
-
-        //enter password, click next
-        WebElement passInput =  waitFor(By.xpath("//input[@type='password']"));
-
-        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
-
-        passInput.sendKeys(pass);
-
-
+        // Dotenv dotenv = Dotenv.load();
+        // String email = dotenv.get("EMAIL");
+        // String pass = dotenv.get("PASS");
+        // //enter email
+        // WebElement emailInput = waitFor(By.id("identifierId"));
+        // emailInput.sendKeys(email);
+        // //click next
+        // sleep(1);
+        // driver.findElement(By.xpath("//span[text()='Next']/..")).click();
+        // //enter password, click next
+        // WebElement passInput =  waitFor(By.xpath("//input[@type='password']"));
+        // driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
+        // passInput.sendKeys(pass);
         //Must click next manually??
         // sleep(5);
         // driver.findElement(By.xpath("//span[text()='Next']/..")).click();
     }
 
-
-    public static String getBankURL(int unit){
+    public static String getBankURL(int unit) {
         List<String> urls = Arrays.asList(
             "https://fuhsd.schoology.com/common-assessment-question-banks/219881976?f=378005801",
             "https://fuhsd.schoology.com/common-assessment-question-banks/220480315?f=378005802",
             "https://fuhsd.schoology.com/common-assessment-question-banks/220480355?f=378005803"
         );
-        return urls.get(unit+1);
+        return urls.get(unit + 1);
     }
 
-    public static void populateQuestion(int questionNum) {
-
-    }
+    public static void populateQuestion(int questionNum) {}
 }
